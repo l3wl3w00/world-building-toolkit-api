c6bffc097b4db307d9babb01516276804704a4ec
Levi <gyorgydeak.levente@gmail.com>
2023-08-08 22:33:56 +0200

    add migrations

---
+++ Dal/Migrations/20230730102117_IntroduceContintents.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntroduceContintents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Radius",
+                table: "Worlds",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.CreateTable(
+                name: "Continent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorldId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Continent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Continent_Worlds_WorldId",
+                        column: x => x.WorldId,
+                        principalTable: "Worlds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorldCoordinate",
+                columns: table => new
+                {
+                    ContinentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Radius = table.Column<float>(type: "real", nullable: false),
+                    Phi = table.Column<float>(type: "real", nullable: false),
+                    Theta = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorldCoordinate", x => new { x.ContinentId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_WorldCoordinate_Continent_ContinentId",
+                        column: x => x.ContinentId,
+                        principalTable: "Continent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Continent_WorldId",
+                table: "Continent",
+                column: "WorldId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorldCoordinate");
+
+            migrationBuilder.DropTable(
+                name: "Continent");
+
+            migrationBuilder.DropColumn(
+                name: "Radius",
+                table: "Worlds");
+        }
+    }
+}
---
+++ Dal/Migrations/20230730104635_ContinentDbSet.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContinentDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Continent_Worlds_WorldId",
+                table: "Continent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorldCoordinate_Continent_ContinentId",
+                table: "WorldCoordinate");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Continent",
+                table: "Continent");
+
+            migrationBuilder.RenameTable(
+                name: "Continent",
+                newName: "Continents");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Continent_WorldId",
+                table: "Continents",
+                newName: "IX_Continents_WorldId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Continents",
+                table: "Continents",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Continents_Worlds_WorldId",
+                table: "Continents",
+                column: "WorldId",
+                principalTable: "Worlds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorldCoordinate_Continents_ContinentId",
+                table: "WorldCoordinate",
+                column: "ContinentId",
+                principalTable: "Continents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Continents_Worlds_WorldId",
+                table: "Continents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorldCoordinate_Continents_ContinentId",
+                table: "WorldCoordinate");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Continents",
+                table: "Continents");
+
+            migrationBuilder.RenameTable(
+                name: "Continents",
+                newName: "Continent");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Continents_WorldId",
+                table: "Continent",
+                newName: "IX_Continent_WorldId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Continent",
+                table: "Continent",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Continent_Worlds_WorldId",
+                table: "Continent",
+                column: "WorldId",
+                principalTable: "Worlds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorldCoordinate_Continent_ContinentId",
+                table: "WorldCoordinate",
+                column: "ContinentId",
+                principalTable: "Continent",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

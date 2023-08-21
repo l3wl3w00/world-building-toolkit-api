6ef6047a5aa29a74f7ce7e18c403d3edd58a4480
Levi <gyorgydeak.levente@gmail.com>
2023-08-21 22:04:24 +0200

    create basic demo test case

--- Test/Integration/Planet/PlanetControllerTests.cs
+++ Test/Integration/Planet/PlanetControllerTests.cs
@@ -1,49 +1,43 @@
 ﻿using Bll.Planet.Dto;
 using Bogus;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Json;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Transactions;
 using FluentAssertions;
 
-namespace Test.Integration.Planet
+namespace Test.Integration.Planet;
+
+public class PlanetControllerTests : IClassFixture<CustomWebApplicationFactory>
 {
-    public class PlanetControllerTests : IClassFixture<CustomWebApplicationFactory>
+    private readonly WebApplicationFactory<Program> _appFactory;
+
+    public PlanetControllerTests(CustomWebApplicationFactory appFactory)
     {
-        private readonly WebApplicationFactory<Program> _appFactory;
+        _appFactory = appFactory;
+    }
 
-        public PlanetControllerTests(CustomWebApplicationFactory appFactory)
-        {
-            _appFactory = appFactory;
-        }
-
-        [Fact]
-        public async Task DemoTest() 
-        {
-            // Arrange
-            _appFactory.Server.PreserveExecutionContext = true;
-            using var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
-            var client = _appFactory.CreateClient();
-            var dto = new Faker<CreatePlanetDto>()
-                .CustomInstantiator(f =>
-                    new
-                    (
-                        Name: "test planet", 
-                        Description: "test description",
-                        CreatorUsername: "nonexistent test user"
-                    )
+    [Fact]
+    public async Task DemoTest() 
+    {
+        // Arrange
+        _appFactory.Server.PreserveExecutionContext = true;
+        using var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+        var client = _appFactory.CreateClient();
+        var dto = new Faker<CreatePlanetDto>()
+            .CustomInstantiator(f =>
+                new
+                (
+                    Name: "test planet", 
+                    Description: "test description",
+                    CreatorUsername: "nonexistent test user"
                 )
-                .Generate();
-            // Act
-            var response = await client.PostAsJsonAsync("/api/planets", dto);
-            var p = await response.Content.ReadFromJsonAsync<PlanetDto>();
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-        }
+            )
+            .Generate();
+        // Act
+        var response = await client.PostAsJsonAsync("/api/planets", dto);
+        var responseDto = await response.Content.ReadFromJsonAsync<PlanetDto>();
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
---
+++ Test/Integration/User/UserControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Transactions;
+using Bll.Auth.Dto;
+using Bll.Planet.Dto;
+using Bogus;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Test.Integration.User;
+
+public class UserControllerTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly WebApplicationFactory<Program> _appFactory;
+
+    public UserControllerTests(CustomWebApplicationFactory appFactory)
+    {
+        _appFactory = appFactory;
+    }
+
+    [Fact]
+    public async Task TestRegister() 
+    {
+        // Arrange
+        _appFactory.Server.PreserveExecutionContext = true;
+        using var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+        var client = _appFactory.CreateClient();
+        
+        var dto = new Faker<RegisterDto>()
+            .CustomInstantiator(f =>
+                new RegisterDto 
+                (
+                    Email: "testemail@gmail.com", 
+                    Username: "TestUserName",
+                    Password: "test pwd"
+                )
+            )
+            .Generate();
+        // Act
+        var response = await client.PostAsJsonAsync("/api/register", dto);
+        var responseDto = await response.Content.ReadFromJsonAsync<RegisterDto>();
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        
+    }
+}
